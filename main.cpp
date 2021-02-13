4c28e172273c66b4e538cea68ad4d2323b9637fe
Ebisu02 <helloimclinker@gmail.com>
2021-02-13 14:45:26 +0700

    Fixed main_cpp, make html code better for eyes

--- main.cpp
+++ main.cpp
@@ -21,40 +21,40 @@ void setFigures(int numFigure, FILE* html_file)
         fprintf(html_file, " ");
         break;
     case 1:
-        fprintf(html_file, "&#9814");
+        fprintf(html_file, "\n\t\t<div class = \"figure\">&#9814;</div>");
         break;
     case 2:
-        fprintf(html_file, "&#9816");
+        fprintf(html_file, "\n\t\t<div class = \"figure\">&#9816;</div>");
         break;
     case 3:
-        fprintf(html_file, "&#9815");
+        fprintf(html_file, "\n\t\t<div class = \"figure\">&#9815;</div>");
         break;
     case 4:
-        fprintf(html_file, "&#9813");
+        fprintf(html_file, "\n\t\t<div class = \"figure\">&#9813;</div>");
         break;
     case 5:
-        fprintf(html_file, "&#9812");
+        fprintf(html_file, "\n\t\t<div class = \"figure\">&#9812;</div>");
         break;
     case 6:
-        fprintf(html_file, "&#9817");
+        fprintf(html_file, "\n\t\t<div class = \"figure\">&#9817;</div>");
         break;
     case 7:
-        fprintf(html_file, "&#9823");
+        fprintf(html_file, "\n\t\t<div class = \"figure\">&#9823;</div>");
         break;
     case 8:
-        fprintf(html_file, "&#9820");
+        fprintf(html_file, "\n\t\t<div class = \"figure\">&#9820;</div>");
         break;
     case 9:
-        fprintf(html_file, "&#9822");
+        fprintf(html_file, "\n\t\t<div class = \"figure\">&#9822;</div>");
         break;
     case 10:
-        fprintf(html_file, "&#9821");
+        fprintf(html_file, "\n\t\t<div class = \"figure\">&#9821;</div>");
         break;
     case 11:
-        fprintf(html_file, "&#9819");
+        fprintf(html_file, "\n\t\t<div class = \"figure\">&#9819;</div>");
         break;
     case 12:
-        fprintf(html_file, "&#9813");
+        fprintf(html_file, "\n\t\t<div class = \"figure\">&#9813;</div>");
         break;
     }
     return;
@@ -64,18 +64,20 @@ void fillAndSetBoard(int board[8][8])
 {
     FILE* html_file;
     html_file = fopen("index.html", "w+");
+    fprintf(html_file, "<!DOCTYPE html>\n<html>\n<head>\n\t<title>Шахматы</title>\n\t<meta charset=\"utf-8\">\n\t<title>Chess</title>\n\t<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\" />\n\t<link rel=\"shortcut icon\" href=\"https://image.flaticon.com/icons/png/512/32/32658.png\"/>\n\t<script src=\"\"></script>\n\t</head>\n<body>\n\t");
     for (int i = 0; i < 8; ++i) {
         Flag = !(Flag);
         for (int j = 0; j < 8; ++j) {
             if (Flag == true)
-                fprintf(html_file, "<div class = \"square white\">");
+                fprintf(html_file, "\n\t<div class = \"square white\">");
             else
-                fprintf(html_file, "<div class = \"square black\">");
+                fprintf(html_file, "\n\t<div class = \"square black\">");
             setFigures(board[i][j], html_file);
-            fprintf(html_file, "</div>");
+            fprintf(html_file, "\n\t</div>");
             Flag = !(Flag);
         }
     }
+    fprintf(html_file, "\n</body>\n</html>");
     return;
 }
 
